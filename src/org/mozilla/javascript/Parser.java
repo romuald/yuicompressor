romuald/yuicompressor

commit 4b9be19a9292a123b55c484a86db5c646d3718c0

Fix for conditional comments - should solve #15, #50, #31

src/org/mozilla/javascript/Parser.java
@@ -2446,6 +2446,16 @@ public class Parser
             reportError("msg.unexpected.eof");
             break;
 
+          case Token.CONDCOMMENT:
+            String s = ts.getString();
+            decompiler.addJScriptConditionalComment(s);
+            return nf.createString(s);
+
+          case Token.KEEPCOMMENT:
+            String s = ts.getString();
+            decompiler.addPreservedComment(s);
+            return nf.createString(s);
+
           default:
             reportError("msg.syntax");
             break;

commit 2ef64bb896a7acf5faa825356f8adeb5f10d3702

Fix parse error introduced by 4b9be19a92

src/org/mozilla/javascript/Parser.java
@@ -2452,9 +2452,9 @@ public class Parser
             return nf.createString(s);
 
           case Token.KEEPCOMMENT:
-            String s = ts.getString();
-            decompiler.addPreservedComment(s);
-            return nf.createString(s);
+            String s2 = ts.getString();
+            decompiler.addPreservedComment(s2);
+            return nf.createString(s2);
 
           default:
             reportError("msg.syntax");